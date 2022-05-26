68489fc41309a538d5baa25ba3b2b161188d1411
Valentinabertinat <valen.bertinat@gmail.com>
2022-05-26 17:14:58 -0300

    segundo commit

--- src/main/java/com/miPortfolio/miPortfolioWeb/controller/HabilidadController.java
+++ src/main/java/com/miPortfolio/miPortfolioWeb/controller/HabilidadController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 import java.util.Optional;
 
-@CrossOrigin
+@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("/habilidad")
 public class HabilidadController {
