18ba0c7b55dd60c3f1d36d15ca58cb0a6666aa10
Valentinabertinat <valen.bertinat@gmail.com>
2022-05-27 21:09:54 -0300

    con cors nuevo

--- Procfile
+++
@@ -1,1 +0,0 @@
-web:java -Server.port=$PORT  -jar target/miPortfolioWeb-0.0.1-SNAPSHOT.jar
--- src/main/java/com/miPortfolio/miPortfolioWeb/MiPortfolioWebApplication.java
+++ src/main/java/com/miPortfolio/miPortfolioWeb/MiPortfolioWebApplication.java
@@ -1,7 +1,12 @@
 package com.miPortfolio.miPortfolioWeb;
 
+import java.util.Arrays;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.filter.CorsFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @SpringBootApplication
 public class MiPortfolioWebApplication {
@@ -10,4 +15,19 @@ public class MiPortfolioWebApplication {
 		SpringApplication.run(MiPortfolioWebApplication.class, args);
 	}
 
+	@Bean
+	public CorsFilter corsFilter(){
+		CorsConfiguration corsConfiguration = new CorsConfiguration();
+		corsConfiguration.setAllowCredentials(true);
+		corsConfiguration.setAllowedOrigins(Arrays.asList("https://porfolio-45c25.web.app"));
+		corsConfiguration.setAllowedHeaders(Arrays.asList("Origin", "Access-Control-Allow-Origin", "Content-Type",
+				"Accept", "Authorization", "Origin, Accept", "X-Requested-With",
+				"Access-Control-Request-Method", "Access-Control-Request-Headers"));
+		corsConfiguration.setExposedHeaders(Arrays.asList("Origin", "Content-Type", "Accept", "Authorization",
+				"Access-Control-Allow-Origin", "Access-Control-Allow-Origin", "Access-Control-Allow-Credentials"));
+		corsConfiguration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+		UrlBasedCorsConfigurationSource urlBasedCorsConfigurationSource = new UrlBasedCorsConfigurationSource();
+		urlBasedCorsConfigurationSource.registerCorsConfiguration("/**", corsConfiguration);
+		return new CorsFilter(urlBasedCorsConfigurationSource);
+	}
 }
